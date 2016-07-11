9e540933bf7dab146d0c7e04999e01bccba743ff
Gabriel Rascovit <gabriel.zeroglosa@gmail.com>
2016-07-11 13:56:47 -0300

    Finalizando implementação do ResolucaoRepositoryJson (persisteTipo, removeTipo, tipoPeloCodigo e tiposPeloNome)

--- src/main/java/br/ufg/inf/persistencia/repositorios/ResolucaoRepositoryJson.java
+++ src/main/java/br/ufg/inf/persistencia/repositorios/ResolucaoRepositoryJson.java
@@ -7,10 +7,14 @@ import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class ResolucaoRepositoryJson implements ResolucaoRepository {
 
     private static final String COLECAO_RESOLUCAO = "resolucao";
+    private static final String COLECAO_TIPO = "tipo";
+    private static final String TIPO_REGRA = "regras.tipoRelato";
+    private static final String NOME_TIPO = "nome";
     private static final String IDENTIFICADOR_UNICO = "id";
 
     public Resolucao byId(String id) {
@@ -57,19 +61,38 @@ public class ResolucaoRepositoryJson implements ResolucaoRepository {
     }
 
     public void persisteTipo(Tipo tipo) {
-
+		Document documentoMongoTipo = GsonHelper.obtenhaDocumentoMongo(tipo);
+	    MongoHelper.persistaDocumentoMongo(COLECAO_TIPO, documentoMongoTipo);
     }
 
     public void removeTipo(String codigo) {
+	    Document filtroPeloId = GsonHelper.obtenhaDocumentoMongo(new Document(TIPO_REGRA, codigo));
+	    Document resolucaoReferenciandoTipo = MongoHelper.recuperaDocumentoMongo(COLECAO_RESOLUCAO, filtroPeloId);
 
+	    if (resolucaoReferenciandoTipo != null) {
+		    throw new ResolucaoUsaTipoException("Não foi possível remover o tipo pois o mesmo é utilizado por pelo menos uma resolução");
+	    }
+
+	    filtroPeloId = GsonHelper.obtenhaDocumentoMongo(new Document(IDENTIFICADOR_UNICO, codigo));
+	    MongoHelper.removeDocumentoMongo(COLECAO_TIPO, filtroPeloId);
     }
 
     public Tipo tipoPeloCodigo(String codigo) {
-        return null;
+	    Document filtroPeloId = GsonHelper.obtenhaDocumentoMongo(new Document(IDENTIFICADOR_UNICO, codigo));
+	    Document documentoMongoTipo = MongoHelper.recuperaDocumentoMongo(COLECAO_TIPO, filtroPeloId);
+        return documentoMongoTipo == null ? null : (Tipo) GsonHelper.obtenhaObjeto(documentoMongoTipo, Tipo.class);
     }
 
     public List<Tipo> tiposPeloNome(String nome) {
-        return null;
+	    List<Document> documentosTiposPeloNome = MongoHelper.recuperaDocumentosMongo(COLECAO_TIPO, new Document(NOME_TIPO, Pattern.compile(nome)));
+	    List<Tipo> tiposPeloNome = new ArrayList<Tipo>();
+
+	    for (Document documentoTipo : documentosTiposPeloNome) {
+		    Tipo tipo = (Tipo) GsonHelper.obtenhaObjeto(documentoTipo, Tipo.class);
+		    tiposPeloNome.add(tipo);
+	    }
+
+	    return tiposPeloNome;
     }
 
 }
--- src/test/java/br/ufg/inf/persistencia/repositorios/ResolucaoRepositoryJsonTest.java
+++ src/test/java/br/ufg/inf/persistencia/repositorios/ResolucaoRepositoryJsonTest.java
@@ -1,13 +1,9 @@
 package br.ufg.inf.persistencia.repositorios;
 
-import br.ufg.inf.es.saep.sandbox.dominio.Regra;
-import br.ufg.inf.es.saep.sandbox.dominio.Resolucao;
+import br.ufg.inf.es.saep.sandbox.dominio.*;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -71,18 +67,85 @@ public class ResolucaoRepositoryJsonTest {
         assertTrue("Os identificadores das resoluções disponíveis são iguais aos persistidos", identificadoresResolucoes.contains(outroIdResolucao));
     }
 
+	@Test
+	public void persisteTipo() {
+		Tipo tipo = obtenhaTipo();
+		String idTipo = tipo.getId();
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		Tipo tipoPersistido = resolucaoRepositoryJson.tipoPeloCodigo(idTipo);
+		assertEquals("O tipo é persistido com sucesso", idTipo, tipoPersistido.getId());
+	}
+
+	@Test
+	public void removeTipo() {
+		Tipo tipo = obtenhaTipo();
+		String idTipo = tipo.getId();
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		resolucaoRepositoryJson.removeTipo(idTipo);
+		assertNull("O tipo foi removido com sucesso", resolucaoRepositoryJson.tipoPeloCodigo(idTipo));
+	}
+
+	@Test(expected = ResolucaoUsaTipoException.class)
+	public void removeTipoFalhaPoisExisteRegraDeResolucaoReferenciandoTipo() {
+		Tipo tipo = obtenhaTipo();
+		String idTipo = tipo.getId();
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		Resolucao resolucao = obtenhaResolucao();
+		Regra regra = new Regra(obtenhaStringAleatoria(), Regra.PONTOS, obtenhaStringAleatoria(), 5.0f, 0.0f, null, null, null, idTipo, 1.0f, null);
+		resolucao.getRegras().add(regra);
+		resolucaoRepositoryJson.persiste(resolucao);
+		resolucaoRepositoryJson.removeTipo(idTipo);
+	}
+
+	@Test
+	public void tipoPeloCodigo() {
+		Tipo tipo = obtenhaTipo();
+		String idTipo = tipo.getId();
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		Tipo tipoPersistido = resolucaoRepositoryJson.tipoPeloCodigo(idTipo);
+		assertEquals("O id do objeto recuperado do banco é igual ao esperado", idTipo, tipoPersistido.getId());
+	}
+
+	@Test
+	public void tipoPeloCodigoNaoEncontraTipoComIdentificadorDiferente() {
+		Tipo tipo = obtenhaTipo();
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		assertNull("O tipo não é encontrado pois o identificador buscado é diferente", resolucaoRepositoryJson.tipoPeloCodigo(obtenhaStringAleatoria()));
+	}
+
+	@Test
+	public void tiposPeloNome() {
+		Tipo tipo = obtenhaTipo();
+		String nomeTipo = tipo.getNome().substring(0, 5);
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		List<Tipo> tiposPersistidos = resolucaoRepositoryJson.tiposPeloNome(nomeTipo);
+		assertEquals("Os tipos que possuem nomes similares foram recuperados com sucesso", tipo.getNome(), tiposPersistidos.get(0).getNome());
+	}
+
+	@Test
+	public void tiposPeloNomeNaoEncontraNadaComNomeDiferente() {
+		Tipo tipo = obtenhaTipo();
+		resolucaoRepositoryJson.persisteTipo(tipo);
+		assertEquals("O método não encontra nenhum tipo pois não existem tipos persistidos com o nome buscado", 0, resolucaoRepositoryJson.tiposPeloNome(obtenhaStringAleatoria()).size());
+	}
+
     private Resolucao obtenhaResolucao() {
         ArrayList<Regra> regras = new ArrayList<Regra>();
         ArrayList<String> dependeDe = new ArrayList<String>();
         dependeDe.add(obtenhaStringAleatoria());
-        Regra regra = new Regra(obtenhaStringAleatoria(), 0, obtenhaStringAleatoria(), 10.0f, 0f, obtenhaStringAleatoria(), obtenhaStringAleatoria(),
-                obtenhaStringAleatoria(), obtenhaStringAleatoria(), 10.0f, dependeDe);
-        Regra outraRegra = new Regra(obtenhaStringAleatoria(), 1, obtenhaStringAleatoria(), 1.0f, -1.0f, obtenhaStringAleatoria(), obtenhaStringAleatoria(),
-                obtenhaStringAleatoria(), obtenhaStringAleatoria(), 10.0f, dependeDe);
+        Regra regra = new Regra(obtenhaStringAleatoria(), Regra.PONTOS, obtenhaStringAleatoria(), 10.0f, 0f, null, null, null, obtenhaStringAleatoria(), 10.0f, null);
+        Regra outraRegra = new Regra(obtenhaStringAleatoria(), Regra.EXPRESSAO, obtenhaStringAleatoria(), 1.0f, -1.0f, obtenhaStringAleatoria(), null, null, null, 0.0f, dependeDe);
         regras.add(regra);
         regras.add(outraRegra);
         return new Resolucao(obtenhaStringAleatoria(), obtenhaStringAleatoria(), obtenhaStringAleatoria(), new Date(), regras);
     }
+
+	private Tipo obtenhaTipo() {
+		Set<Atributo> atributos = new HashSet<Atributo>();
+		atributos.add(new Atributo(obtenhaStringAleatoria(), obtenhaStringAleatoria(), Atributo.STRING));
+		atributos.add(new Atributo(obtenhaStringAleatoria(), obtenhaStringAleatoria(), Atributo.STRING));
+		return new Tipo(obtenhaStringAleatoria(), obtenhaStringAleatoria(), obtenhaStringAleatoria(), atributos);
+	}
 
     private String obtenhaStringAleatoria() {
         return UUID.randomUUID().toString();
